bf78f026f2ce34052ef761c6a0af8bc53b309c18
Vitak84 <122166139+Vitak84@users.noreply.github.com>
2023-05-28 13:27:42 +0000

    Deploying to gh-pages from @ Vitak84/goit-js-hw-08@f1d7c3eae5fc3b513b4fe70edf86371b0afe3bb1 🚀

---
+++ 03-feedback.1aaf8819.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".feedback-form"),t=(document.querySelector(".feedback-form input"),document.querySelector(".feedback-form textarea"),{});e.addEventListener("input",(e=>{t[e.target.name]=e.target.value,localStorage.setItem("feedback-form-state",JSON.stringify(t))}));
+//# sourceMappingURL=03-feedback.1aaf8819.js.map
---
+++ 03-feedback.4850f622.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".feedback-form"),t=(document.querySelector(".feedback-form input"),document.querySelector(".feedback-form textarea"),"feedback-form-state"),r={};e.addEventListener("input",(function(e){r[e.target.name]=e.target.value,localStorage.setItem(t,JSON.stringify(r))}))}();
+//# sourceMappingURL=03-feedback.4850f622.js.map
